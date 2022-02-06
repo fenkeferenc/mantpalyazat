3b047d9944c9d28085f2f50f1aef0d34f14bd3f0
Fenke Ferenc <68777033+Kredenc8000@users.noreply.github.com>
2022-02-06 15:53:33 +0100

    kesz

--- app.py
+++ app.py
@@ -31,10 +31,14 @@ def img(x):
     else:
         return "<img src=" + os.path.join(x)+'>'
 
+def errormsg():
+    return render_template('index.html',click="Return to the main page", wiki="Error <br> <br> Please return to the home page.")
+
 @app.route('/')
 def form():
     home = render_template('index.html', image = "<img src=" + os.path.join(app.config['UPLOAD_FOLDER'], "epic" + str(randint(1,3)) +".jpg")+">", 
-    wiki= "<br> + date/time <br> + weather <br> + weather on the Mars <br> + Picture of the day *on [Year-Month-Day]* <br> + International Space Station, ISS <br> + Search (using wikipedia) *title* <br> + News(Latest space exploration news)", TextTitle = "<h1> The keyword list </h1>")
+    wiki= "<br> + date/time <br> + weather <br> + weather on the Mars <br> + Picture of the day *on [Year-Month-Day]* <br> + International Space Station, ISS <br> + Search (using wikipedia) *title* <br> + News(Latest space exploration news)", TextTitle = "<h1> Keyword list </h1>",
+    doc = "This is the main page of the program. ")
     return home
 
 @app.route('/frame')
@@ -57,13 +61,15 @@ def main():
             out = ""
             for i in range(0,len(explain)):
                 out += explain[i] + "."
-            return render_template('index.html', image = img(gtt["url"]), wiki = out)
+            return render_template('index.html',click="Return to the main page", image = img(gtt["url"]), wiki = out)
         elif "no" in user_input:
             apod = 0
-            return render_template('index.html', wiki = "Okay.")
+            return render_template('index.html',click="Return to the main page", wiki = "Okay.")
+        elif "home" in user_input:
+            return form()
         else:
             apod = 1
-            return render_template('index.html', image = img(gtt["url"]), wiki = "Answer with yes or no please!")
+            return render_template('index.html',click="Return to the main page", image = img(gtt["url"]), wiki = "Answer with yes or no please!")
 
     elif "time" in user_input:
         if "in" in user_input:
@@ -75,9 +81,9 @@ def main():
         if "date" in user_input:
             now = datetime.now()
             current_date = now.strftime("%D")
-            return  render_template('index.html', wiki = " The current time is: " + current_time + "<br> And the current date is: " + current_date)
+            return  render_template('index.html',click="Return to the main page", wiki = " The current time is: " + current_time + "<br> And the current date is: " + current_date)
         else:
-            return render_template('index.html', wiki="The current time is: " + current_time)
+            return render_template('index.html',click="Return to the main page", wiki="The current time is: " + current_time)
     
     elif "home" in user_input:
         return form()
@@ -85,27 +91,30 @@ def main():
     elif "date" in user_input:
         now = datetime.now()
         current_date = now.strftime("%D")
-        return render_template('index.html', wiki="The current date is: " + current_date)
+        return render_template('index.html', click="Return to the main page",wiki="The current date is: " + current_date)
 
     elif "weather" in user_input and "mars" in user_input:
-        f = r"https://api.maas2.apollorion.com"
-        response = requests.get(f)
-        result = json.loads(response.text)
-        min_temp = int(result["min_temp"])
-        max_temp = int(result["max_temp"])
-        avg_temp = (min_temp + max_temp)/2
-        avg_temp = str(avg_temp)
-        t = str(result["terrestrial_date"])
-        pressure = str(result["pressure"])
-        opacity = str(result["atmo_opacity"])
-        sol = str(result["sol"])
-        timestamp = str(t.split("T")[0])
-        return render_template('index.html', wiki="The average temperature on Mars in this month was: <br>"+ avg_temp + "C <br><br>" +
-                                "The weather according to the opacity of the atmosphere is: <br>" + opacity + "<br><br>" + 
-                                "Average atmospheric pressure: <br>" + pressure + " Pascal <br><br>" +
-                                "This was the " + sol +". "+"report since 2012-08-07" +"<br>"+
-                                "Updated on: " + timestamp,
-                                image = img("/static/img/rover1.png"))
+        try:
+            f = r"https://api.maas2.apollorion.com"
+            response = requests.get(f)
+            result = json.loads(response.text)
+            min_temp = int(result["min_temp"])
+            max_temp = int(result["max_temp"])
+            avg_temp = (min_temp + max_temp)/2
+            avg_temp = str(avg_temp)
+            t = str(result["terrestrial_date"])
+            pressure = str(result["pressure"])
+            opacity = str(result["atmo_opacity"])
+            sol = str(result["sol"])
+            timestamp = str(t.split("T")[0])
+            return render_template('index.html', click="Return to the main page",wiki="The average temperature on Mars in this month was: <br>"+ avg_temp + "C <br><br>" +
+                                    "The weather according to the opacity of the atmosphere is: <br>" + opacity + "<br><br>" + 
+                                    "Average atmospheric pressure: <br>" + pressure + " Pascal <br><br>" +
+                                    "This was the " + sol +". "+"report since 2012-08-07" +"<br>"+
+                                    "Updated on: " + timestamp,
+                                    image = img("/static/img/rover1.png"))
+        except:
+            errormsg()
 
 
     #--------------------------------Weather--------------------------------#
@@ -115,35 +124,37 @@ def main():
             city_name = city[1]
         else:
             city_name = "Kourou"
+        try:
+            base_url = "http://api.openweathermap.org/data/2.5/weather?"
+            complete_url = base_url + "appid=" + w_api_key + "&q=" + city_name
+            response = requests.get(complete_url)
+            x = response.json()
+    
+            if x["cod"] != "404":
+                y = x["main"]
+    
+                current_temperature = y["temp"]
+                # ----------------------------------------#
+                celsius = current_temperature - 272, 15
+                # ----------------------------------------#
+                z = x["weather"]
 
-        base_url = "http://api.openweathermap.org/data/2.5/weather?"
-        complete_url = base_url + "appid=" + w_api_key + "&q=" + city_name
-        response = requests.get(complete_url)
-        x = response.json()
+                weather_description = z[0]["description"]
+                if "cloud" in weather_description:
+                    weather_descriptionm = "clouds"
+                elif "clear" in weather_description:
+                    weather_descriptionm = "clear"
+                elif "rain" in weather_description:
+                    weather_descriptionm = "rain"
+                elif "fog" or "mist" in weather_description:
+                    weather_descriptionm = "mist"
+    
+                w = weather_descriptionm + str(randint(1,3))+".png" 
+                imgpath = os.path.join(app.config['UPLOAD_FOLDER'], w)
 
-        if x["cod"] != "404":
-            y = x["main"]
-
-            current_temperature = y["temp"]
-            # ----------------------------------------#
-            celsius = current_temperature - 272, 15
-            # ----------------------------------------#
-            z = x["weather"]
-            
-            weather_description = z[0]["description"]
-            if "cloud" in weather_description:
-                weather_descriptionm = "clouds"
-            elif "clear" in weather_description:
-                weather_descriptionm = "clear"
-            elif "rain" in weather_description:
-                weather_descriptionm = "rain"
-            elif "fog" or "mist" in weather_description:
-                weather_descriptionm = "mist"
-
-            w = weather_descriptionm + str(randint(1,3))+".png" 
-            imgpath = os.path.join(app.config['UPLOAD_FOLDER'], w)
-            
-            return render_template('index.html', image = img(imgpath), wiki = "The current temperature in " + city_name + " is: " + str(celsius).replace('(', '')[:3] + "C°" + "<br>" + "The weather in " + city_name + " is: " + str(weather_description))
+                return render_template('index.html',click="Return to the main page", image = img(imgpath), wiki = "The current temperature in " + city_name + " is: " + str(celsius).replace('(', '')[:3] + "C°" + "<br>" + "The weather in " + city_name + " is: " + str(weather_description))
+        except:
+            errormsg()
     #--------------------------------Weather--------------------------------#
     elif "search" in user_input:
         try:
@@ -151,9 +162,9 @@ def main():
             data = lowcase.replace("search ", "")
             data = data.replace("for", "")
             new = data.translate({ord(i): None for i in ' '})
-            return render_template('index.html', wiki = wikipedia.summary(new, sentences=5))
+            return render_template('index.html',click="Return to the main page", wiki = wikipedia.summary(new, sentences=5))
         except:
-            return render_template('index.html', wiki = "Wikipedia page was not found, type 'home' to return to the home page")
+            return render_template('index.html', click="Return to the main page", wiki = "Wikipedia page was not found, type 'home' to return to the home page")
 
     elif "picture of the day" in user_input or "apod" in user_input:
         if "on" in user_input:
@@ -167,7 +178,7 @@ def main():
             data = requests.get(f,params=params)
             gtt = json.loads(data.text)
             print(gtt)
-            return render_template('index.html', wiki = "The astronomy picture on " + on + "was: " + "<br> <b>" + gtt["title"] + " </b> <br> <br>" +
+            return render_template('index.html', click="Return to the main page", wiki = "The astronomy picture on " + on + "was: " + "<br> <b>" + gtt["title"] + " </b> <br> <br>" +
                                     "Do you want to hear the explanation of this picture?", image = img(gtt["url"]))
 
         elif "on" not in user_input:
@@ -175,7 +186,7 @@ def main():
             f = r"https://api.nasa.gov/planetary/apod?api_key=XMqdRJg4lgeRUm0N1ETvfUvymjgmfhXJ7ot2Udgj"
             data = requests.get(f)
             gtt = json.loads(data.text)
-            return render_template('index.html', wiki = "The astronomy picture of the day is: " + "<br> <b>" + gtt["title"] + " </b> <br> <br>" +
+            return render_template('index.html', click="Return to the main page", wiki = "The astronomy picture of the day is: " + "<br> <b>" + gtt["title"] + " </b> <br> <br>" +
                                     "Do you want to hear the explanation of this picture?", image = img(gtt["url"]))
         
     elif "international space station" in user_input or "iss" in user_input:
@@ -193,11 +204,11 @@ def main():
         country = pos.get("cc")
         country = str(get_country_name(country))
         position = "The International Space Station is currently over " + state + " in " + country
-        return render_template('index.html', wiki=position, image='<iframe src="frame"></iframe>')
+        return render_template('index.html',click="Return to the main page", wiki=position, image='<iframe src="frame"></iframe>')
 
     elif "news" in user_input:
         title, summary, url, imgurl, = news()[0], news()[1], news()[2], news()[3]
-        return render_template('index.html', TextTitle="<h1>"+ '<a href="'+url+'">'  + title + "</a></h1>" + "<br>", wiki=summary, image='<img src="' + imgurl + '">'+'</a>',)
+        return render_template('index.html', click="Return to the main page", TextTitle='<h1 id="link">'+ '<a href="'+url+'">'  + title + "</a></h1>" + "<br>", wiki=summary, image='<img src="' + imgurl + '">'+'</a>',)
 
     return form()
 
